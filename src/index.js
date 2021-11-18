023bf6b4aea1318d86f4ab479cd1febcdbee6de3
Sarnyai Ákos <sarnyaia1@gmail.com>
2021-11-18 13:58:11 +0100

    add pwa files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from './App';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDom.render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
